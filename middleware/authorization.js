c7288d7a2ae8c6f886a96b7a5912912a2c86da74
arburhan <burhan128454@outlook.com>
2023-02-11 14:52:20 +0600

    create authorization method

---
+++ middleware/authorization.js
@@ -0,0 +1,12 @@
+exports = (...role) => {
+    return (req, res, next) => {
+        const userRole = req.user.role;
+        if (!role.includes(userRole)) {
+            return res.status(403).json({
+                status: "fail",
+                error: "you have no access"
+            })
+        }
+        next();
+    }
+}
